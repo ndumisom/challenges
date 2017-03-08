4311de7e174afa8e84e1dd4a0c7671298bce9050
ndumiso <nmbete@tagworldwide.com>
2017-03-09 01:13:16 +0200

    Add solutions for all 4 challenges

--- Challenge3/api/config.php
+++ Challenge3/api/config.php
@@ -1,9 +1,9 @@
 <?php
 function getDB() {
-	$dbhost="localhost";
-	$dbuser="root";
-	$dbpass="ayabonga";
-	$dbname="challenge3db";
+	$dbhost="";
+	$dbuser="";
+	$dbpass="";
+	$dbname="";
 	$dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);	
 	$dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	return $dbConnection;
